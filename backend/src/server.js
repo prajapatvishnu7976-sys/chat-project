6192d97ed1ada15fd2bb5d7597a861ae0f94417f
prajapatvishnu7976-sys <prajapatvishnu7976@gmail.com>
2026-03-25 00:34:38 +0530

    Updated production URLs

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,14 +13,23 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json({ limit: "5mb" })); // req.body
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(express.json({ limit: "5mb" }));
+
+// CORS - Production URLs
+app.use(cors({ 
+  origin: [
+    "http://localhost:5173",
+    "https://chat-project-three-rho.vercel.app"
+  ],
+  credentials: true 
+}));
+
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// make ready for deployment
+// Make ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
